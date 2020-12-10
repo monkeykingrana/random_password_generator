92a9e06fd3a5577fd97ba4b8c431552bc643f525
S.Rana <yashsachin1998@gmail.com>
2020-12-10 13:23:16 +0530

    working on 1st part

--- random_password_generator.py
+++ random_password_generator.py
@@ -1,36 +1,52 @@
 import random
 import string
-print('HEllO DEAR\nWhat Do You Want To Do\n1. Generate New Password\n2. SAVED PASSWORDS\n\n')
-usr_input_1= input('==>Enter Your Choice :')
+print('''HEllO DEAR
 
+What Do You Want To Do
+
+1. Generate New Password
+
+2. SAVED PASSWORDS
+
+''')
+usr_input_1= input('==>Enter Your Choice :')
 
 
 string.result =''
 if usr_input_1=='1':
     usr_input_1_1=input('==Enter Length For Your Password==\n\n==>acceptable inputs are : 6,7,8,9,10 : ')
-    len=usr_input_1_1
-    def get_random_string(len):
+    def get_random_string(usr_input_1_1):
+        sample_puntuations='*@#_$&'
 
-        random_source = string.ascii_letters + string.digits
+        random_source = string.ascii_letters + string.digits + sample_puntuations
         password = random.choice(string.ascii_lowercase)
         password += random.choice(string.ascii_uppercase)
         password += random.choice(string.digits)
-        password += random.choice(string.punctuation)
-        string.result = ''.join((random.choice(password) for i in range (len)))
-
+        password += random.choice(sample_puntuations)
+        string.result = ''.join((random.choice(password) for i in range (usr_input_1_1)))
+        print(string.result)
+    get_random_string(int(usr_input_1_1))
     if usr_input_1_1=='6':
         print('Thank You\nYour Input Is "6"')
-        get_random_string()
+
         print("Your Generated Password Is : ",string.result)
 
     elif usr_input_1_1=='7':
         print('Thank You\nYour Input Is "7"')
+        print("Your Generated Password Is : ",string.result)
+
     elif usr_input_1_1=='8':
         print('Thank You\nYour Input Is "8"')
+        print("Your Generated Password Is : ",string.result)
+
     elif usr_input_1_1=='9':
         print('Thank You\nYour Input Is "9"')
+        print("Your Generated Password Is : ",string.result)
+
     elif usr_input_1_1=='10':
         print('Thank You\nYour Input Is "10"')
+        print("Your Generated Password Is : ",string.result)
+
     else:
         print('\n===> SORRY INVALID INPUT <===\n')
         print('====> TRY AGAIN PLEASE <====\n')
