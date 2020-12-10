d5424623f616ba5400b63551d54faffc9e4fbc15
S.Rana <yashsachin1998@gmail.com>
2020-12-10 14:46:03 +0530

    step 1 finalize

--- random_password_generator.py
+++ random_password_generator.py
@@ -1,55 +1,42 @@
 import random
 import string
-print('''HEllO DEAR
-
+from os import system, name
+def clear():
+	if name == 'nt':
+		_ = system('cls')
+clear()
+print('''\nHEllO DEAR
 What Do You Want To Do
-
 1. Generate New Password
-
 2. SAVED PASSWORDS
-
 ''')
-usr_input_1= input('==>Enter Your Choice :')
-
-
+usr_input_1= input('\n==>Enter Your Choice :')
+clear()
 string.result =''
 if usr_input_1=='1':
-    usr_input_1_1=input('==Enter Length For Your Password==\n\n==>acceptable inputs are : 6,7,8,9,10 : ')
+    usr_input_1_1=input('\n==Enter Length For Your Password==\n\n==>acceptable inputs are : 6,7,8,9,10 : ')
     def get_random_string(usr_input_1_1):
         sample_puntuations='*@#_$&'
-
         random_source = string.ascii_letters + string.digits + sample_puntuations
         password = random.choice(string.ascii_lowercase)
         password += random.choice(string.ascii_uppercase)
         password += random.choice(string.digits)
         password += random.choice(sample_puntuations)
         string.result = ''.join((random.choice(password) for i in range (usr_input_1_1)))
-        print(string.result)
     get_random_string(int(usr_input_1_1))
+    clear()
     if usr_input_1_1=='6':
-        print('Thank You\nYour Input Is "6"')
-
-        print("Your Generated Password Is : ",string.result)
-
+        print('\nThank You\nYour Input Is '+'"'+ usr_input_1_1 + '"' +"\nYour Generated Password Is : ",string.result)
     elif usr_input_1_1=='7':
-        print('Thank You\nYour Input Is "7"')
-        print("Your Generated Password Is : ",string.result)
-
+        print('\nThank You\nYour Input Is '+'"'+ usr_input_1_1 + '"' +"\nYour Generated Password Is : ",string.result)
     elif usr_input_1_1=='8':
-        print('Thank You\nYour Input Is "8"')
-        print("Your Generated Password Is : ",string.result)
-
+        print('\nThank You\nYour Input Is '+'"'+ usr_input_1_1 + '"' +"\nYour Generated Password Is : ",string.result)
     elif usr_input_1_1=='9':
-        print('Thank You\nYour Input Is "9"')
-        print("Your Generated Password Is : ",string.result)
-
+        print('\nThank You\nYour Input Is '+'"'+ usr_input_1_1 + '"' +"\nYour Generated Password Is : ",string.result)
     elif usr_input_1_1=='10':
-        print('Thank You\nYour Input Is "10"')
-        print("Your Generated Password Is : ",string.result)
-
+        print('\nThank You\nYour Input Is '+'"'+ usr_input_1_1 + '"' +"\nYour Generated Password Is : ",string.result)
     else:
-        print('\n===> SORRY INVALID INPUT <===\n')
-        print('====> TRY AGAIN PLEASE <====\n')
+        print('\n===> SORRY INVALID INPUT <===\n====> TRY AGAIN PLEASE <====\n')
 
 
 elif usr_input_1=='2':
